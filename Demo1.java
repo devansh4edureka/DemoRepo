9c6857913425d5747e02e142787eb311204d8cc3
devansh4edureka <71836019+devansh4edureka@users.noreply.github.com>
2020-12-30 22:11:06 +0530

    Added a new line to Demo1 from remote repo

--- Demo1.java
+++ Demo1.java
@@ -4,5 +4,6 @@ public static void main (String args[])
 {
 	System.out.println("Java Demo Class 1");
 	System.out.println("This is second line from Demo class 1");
+	System.out.println("This is third line from Demo class 1 committed on remote repo");
 }
 }
